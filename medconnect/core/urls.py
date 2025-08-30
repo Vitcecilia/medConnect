ce198693251c096527c41f29c093958013f8cdfc
William Mattson Kheel Gomes Araújo <157336038+william-araujo456@users.noreply.github.com>
2025-08-29 20:48:41 -0400

    commit add and fix models, serializers, filters, urls, viewsets and imports
    * commit fix all choices
    
    * commit fix all choices
    
    * commit add serializers, urls and viewsets
    
    * fix serializers, urls and viewsets imports
    
    * fix filters

---
+++ medconnect/core/urls.py
@@ -0,0 +1,18 @@
+from rest_framework import routers
+
+from core import viewsets
+
+router = routers.DefaultRouter()
+
+router.register('users', viewsets.UserViewSet)
+router.register('telephones', viewsets.TelephoneViewSet)
+router.register('addresses', viewsets.AddressViewSet)
+router.register('health_insurances', viewsets.HealthInsuranceViewSet)
+router.register('patients', viewsets.PatientViewSet)
+router.register('medics', viewsets.MedicViewSet)
+router.register('specializations', viewsets.SpecializationViewSet)
+router.register('consultations', viewsets.ConsultationViewSet)
+router.register('medications', viewsets.MedicationViewSet)
+router.register('medication_consultations', viewsets.MedicationConsultationViewSet)
+
+urlpatterns = router.urls
